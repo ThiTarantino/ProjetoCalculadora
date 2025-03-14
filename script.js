ac9ec841a1b968336edbc2a1ac8ac871455ed138
ThiTarantino <thiagoatarantino@gmail.com>
2025-03-14 15:15:25 -0300

    Calculadora Pronta

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function insert(num){
-    document.getElementById('texto').innerHTML = num;
+    var texto = document.getElementById('texto').innerHTML;
+    if (texto === '0') {
+        document.getElementById('texto').innerHTML = num;
+    } else {
+        document.getElementById('texto').innerHTML = texto + num;
+    }
+}
+function clean(){
+    document.getElementById('texto').innerHTML= '0';
+    document.getElementById('texto2').innerHTML = '0';
+}
+function back(){
+    var texto = document.getElementById('texto').innerHTML;
+    document.getElementById('texto').innerHTML= texto.substring(0,texto.length -1);
+}
+function calcular(){
+    var texto = document.getElementById('texto').innerHTML;
+    var texto = eval(texto);
+    document.getElementById('texto2').innerHTML = texto; 
+    document.getElementById('texto').innerHTML = '0';
+    
 }
